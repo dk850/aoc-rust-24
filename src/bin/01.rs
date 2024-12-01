5bc56bfe78bb5d597c60cf6415d617fc4a8b24da
= <=>
2024-12-01 17:52:39 +0000

    Day 1 Part 2 solved

--- src/bin/01.rs
+++ src/bin/01.rs
@@ -25,13 +25,35 @@ pub fn part_one(input: &str) -> Option<u32> {
     // Sum and get answer
     let sum: u32 = match i32::try_into(diffs.iter().sum()) {
         Ok(u32) => u32,
-        Err(e) => 0,
+        Err(_) => 0,
     };
     return Some(sum);
 }
 
 pub fn part_two(input: &str) -> Option<u32> {
-    None
+    let mut lhs: Vec<u32> = Vec::new();
+    let mut rhs: Vec<u32> = Vec::new();
+
+    // Build left and right hand side lists
+    for id in input.split("\n") {
+        let line: Vec<String> = id.split_whitespace().map(str::to_string).collect();
+        lhs.push(line.get(0)?.parse().unwrap());
+        rhs.push(line.get(1)?.parse().unwrap());
+    }
+
+    // Count occurances of LHS in RHS and sum as per day logic
+    // Might be faster sorting first and somehow telling rust its searching a sorted list when counting occurrences. Didn't bother optimising
+    let mut sum_similarity_score: u32 = 0;
+    for (_, lhs_elem) in lhs.iter_mut().enumerate() {
+        let this_count: u32 = rhs
+            .iter()
+            .filter(|&val| *val == *lhs_elem)
+            .count()
+            .try_into()
+            .unwrap(); // count occurrences of a filter that returns true for when rhs 'val' is the val of the lhs element
+        sum_similarity_score += *lhs_elem * this_count; // day logic
+    }
+    return Some(sum_similarity_score);
 }
 
 #[cfg(test)]
@@ -47,6 +69,6 @@ mod tests {
     #[test]
     fn test_part_two() {
         let result = part_two(&advent_of_code::template::read_file("examples", DAY));
-        assert_eq!(result, None);
+        assert_eq!(result, Some(31));
     }
 }
