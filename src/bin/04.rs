f207af7b919cfa16ce142c5533c10a10438347b8
Dean Kenny <dean.kenny@uk.thalesgroup.com>
2024-12-04 13:31:46 +0000

    Day 4 pt 1 good - pt 2 horrible with my methods

--- src/bin/04.rs
+++ src/bin/04.rs
@@ -1,14 +1,16 @@
 use core::str;
-
-use advent_of_code::template::aoc_cli::check;
+use strum::IntoEnumIterator;
+use strum_macros::EnumIter;
 
 advent_of_code::solution!(4);
 
+#[derive(Debug, PartialEq, Clone)]
 struct GridPos {
     x: usize,
     y: usize,
 }
 
+#[derive(Debug, EnumIter)]
 enum GridDir {
     Up,
     Down,
@@ -22,6 +24,7 @@ enum GridDir {
 
 pub fn parse_into_grid(input: &str) -> Vec<Vec<String>> {
     let mut output: Vec<Vec<String>> = Vec::new();
+
     // Build grid
     for line in input.lines() {
         output.push(line.chars().map(String::from).collect());
@@ -29,104 +32,238 @@ pub fn parse_into_grid(input: &str) -> Vec<Vec<String>> {
     output
 }
 
-pub fn check_letter(grid: &Vec<Vec<String>>, letter: String, at_pos: &GridPos) -> bool {
-    println!(
-        "Checking letter {} at pos {}, {}",
-        letter, at_pos.x, at_pos.y
-    );
-    grid[at_pos.y][at_pos.x] == letter
-}
-
-pub fn check_word(
+// Returns the mid-point or Error
+fn check_word(
     grid: &Vec<Vec<String>>,
-    word: String,
-    at_pos: &GridPos,
-    direction: GridDir,
-) -> bool {
+    word: &String,
+    start_pos: &GridPos,
+    direction: &GridDir,
+) -> Option<GridPos> {
+    let max_y = grid.len()-1;
+    let max_x = grid.first().unwrap_or(&Vec::new()).len()-1;
+    let word_len = word.len()-1;
+
+    // If word is odd return the mid point instead of start point  [MAS vs XMAS]
+    let mut point_modifier = 0;
+    if word.len() % 2 == 1 {
+        point_modifier = (word.len() - 1) / 2;
+    }
+    let mut ret_pos = GridPos {
+        x: start_pos.x,
+        y: start_pos.y,
+    };
+
+    // Match on direction, error check, then check each letter matches. For each direction
     match direction {
         GridDir::Up => {
-            println!("Checking up");
-            for letter in 0..word.len() {
-                let check_pos = GridPos {
-                    x: at_pos.x,
-                    y: at_pos.y - letter,
-                };
-                let abc = (word.as_bytes()[letter] as char).to_string();
-                println!("LETTER {} of {} is {}", letter, word, abc);
-                if !check_letter(grid, abc, &check_pos) {
-                    return false;
+            if start_pos.y.checked_sub(word_len) == None {
+                return None
+            }
+            for (letter_pos, &letter_bytes) in word.as_bytes().iter().enumerate() {
+                if !(grid[start_pos.y - letter_pos][start_pos.x] == (letter_bytes as char).to_string()) {
+                    return None;
                 }
             }
+            ret_pos.y -= point_modifier;
+            return Some(ret_pos);
         }
-        _ => todo!(),
+
+        GridDir::Down => {
+            if (start_pos.y.checked_add(word_len) == None) || ((start_pos.y + word_len) > max_y) {
+                return None
+            }
+            for (letter_pos, &letter_bytes) in word.as_bytes().iter().enumerate() {
+                if !(grid[start_pos.y + letter_pos][start_pos.x] == (letter_bytes as char).to_string()) {
+                    return None;
+                }
+            }
+            ret_pos.y += point_modifier;
+            return Some(ret_pos);
+        }
+
+        GridDir::Left => {
+            if start_pos.x.checked_sub(word_len) == None {
+                return None
+            }
+            for (letter_pos, &letter_bytes) in word.as_bytes().iter().enumerate() {
+                if !(grid[start_pos.y][start_pos.x - letter_pos] == (letter_bytes as char).to_string()) {
+                    return None;
+                }
+            }
+            ret_pos.x -= point_modifier;
+            return Some(ret_pos);
+        }
+
+        GridDir::Right => {
+            if (start_pos.x.checked_add(word_len) == None) || ((start_pos.x + word_len) > max_x) {
+                return None
+            }
+            for (letter_pos, &letter_bytes) in word.as_bytes().iter().enumerate() {
+                if !(grid[start_pos.y][start_pos.x + letter_pos] == (letter_bytes as char).to_string()) {
+                    return None;
+                }
+            }
+            ret_pos.x += point_modifier;
+            return Some(ret_pos);
+        }
+
+        GridDir::ULeft => {
+            if (start_pos.y.checked_sub(word_len) == None) || (start_pos.x.checked_sub(word_len) == None) {
+                return None
+            }
+            for (letter_pos, &letter_bytes) in word.as_bytes().iter().enumerate() {
+                if !(grid[start_pos.y - letter_pos][start_pos.x - letter_pos] == (letter_bytes as char).to_string()) {
+                    return None;
+                }
+            }
+            ret_pos.y -= point_modifier;
+            ret_pos.x -= point_modifier;
+            return Some(ret_pos);
+        }
+
+        GridDir::URight => {
+            if (start_pos.y.checked_sub(word_len) == None)
+                || (start_pos.x.checked_add(word_len) == None) || ((start_pos.x + word_len) > max_x)
+            {
+                return None
+            }
+            for (letter_pos, &letter_bytes) in word.as_bytes().iter().enumerate() {
+                if !(grid[start_pos.y - letter_pos][start_pos.x + letter_pos] == (letter_bytes as char).to_string()) {
+                    return None;
+                }
+            }
+            ret_pos.y -= point_modifier;
+            ret_pos.x += point_modifier;
+            return Some(ret_pos);
+        }
+
+        GridDir::DLeft => {
+            if (start_pos.y.checked_add(word_len) == None) || ((start_pos.y + word_len) > max_y)
+                || (start_pos.x.checked_sub(word_len) == None) {
+                return None
+            }
+            for (letter_pos, &letter_bytes) in word.as_bytes().iter().enumerate() {
+                if !(grid[start_pos.y + letter_pos][start_pos.x - letter_pos] == (letter_bytes as char).to_string()) {
+                    return None;
+                }
+            }
+            ret_pos.y += point_modifier;
+            ret_pos.x -= point_modifier;
+            return Some(ret_pos);
+        }
+
+        GridDir::DRight => {
+            if (start_pos.y.checked_add(word_len) == None) || ((start_pos.y + word_len) > max_y)
+                || (start_pos.x.checked_add(word_len) == None) || ((start_pos.x + word_len) > max_x) {
+                return None
+            }
+            for (letter_pos, &letter_bytes) in word.as_bytes().iter().enumerate() {
+                if !(grid[start_pos.y + letter_pos][start_pos.x + letter_pos] == (letter_bytes as char).to_string()) {
+                    return None;
+                }
+            }
+            ret_pos.y += point_modifier;
+            ret_pos.x += point_modifier;
+            return Some(ret_pos);
+        }
     }
-    false
 }
 
-pub fn find_word_in_grid(grid: &Vec<Vec<String>>, word: String, at_pos: &GridPos) -> i32 {
-    // LOGIC : Check the word can fit in the direction, then for each letter, move an extra space and see if its there
-    let mut found_count: u32;
-    let word_len = word.len();
-    let max_y = grid.len();
-    let max_x = grid.first().unwrap_or(&Vec::new()).len();
-
-    // Look up
-    match at_pos.y.checked_sub(word_len - 1) {
-        None => println!("{}, {} nofit", at_pos.x, at_pos.y),
-        _ => {
-            println!("{}, {} fit", at_pos.x, at_pos.y);
-            check_word(grid, word, at_pos, GridDir::Up);
+fn find_word_in_grid(grid: &Vec<Vec<String>>, word: &String, at_pos: &GridPos) -> i32 {
+    // Check if there is a match in each direction
+    let mut found_count: i32 = 0;
+    for dir in GridDir::iter()
+    {
+        if check_word(grid, word, at_pos, &dir).is_some() {
+            found_count += 1;
         }
     }
+    found_count
+}
 
-    // Look down
+fn find_cross_word_in_grid(grid: &Vec<Vec<String>>, word: &String, at_pos: &GridPos) -> Vec<GridPos> {
+    // Check if there is a match in each diagonal. Return positive match positions for futher filtering
+    let mut found_pos: Vec<GridPos> = Vec::new();
 
-    // Look left
+    let ul = check_word(grid, word, at_pos, &GridDir::ULeft);
+    if ul.is_some() {
+        found_pos.push(ul.unwrap());
+    }
 
-    // Look right
+    let ur = check_word(grid, word, at_pos, &GridDir::URight);
+    if ur.is_some() {
+        found_pos.push(ur.unwrap());
+    }
 
-    // Look up-left diagonal
+    let dl = check_word(grid, word, at_pos, &GridDir::DLeft);
+    if dl.is_some() {
+        found_pos.push(dl.unwrap());
+    }
 
-    // Look up-right diagonal
-
-    // Look down-left diagonal
-
-    // Look down-right diagonal
-    0
+    let dr = check_word(grid, word, at_pos, &GridDir::DRight);
+    if dr.is_some() {
+        found_pos.push(dr.unwrap());
+    }
+    found_pos
 }
 
 pub fn part_one(input: &str) -> Option<u32> {
+    let search_term = "XMAS";
     let input_grid = parse_into_grid(input);
-    println!("Input Wordsearch:");
-    advent_of_code::print_2d_vec(&input_grid);
+
+    // println!("Input Wordsearch:");
+    // advent_of_code::print_2d_vec(&input_grid);
 
     // Iterate over grid to find every occurrence of X (starting letter of search term)
-    let mut x_pos: Vec<GridPos> = Vec::new(); // [1, 2], [0, 0]...
+    let first_letter = search_term.chars().next().unwrap().to_string();
+    let mut first_letter_pos: Vec<GridPos> = Vec::new(); // [1, 2], [0, 0]...
     for (y, row) in input_grid.iter().enumerate() {
         for (x, letter) in row.iter().enumerate() {
-            if letter == "X" {
-                x_pos.push(GridPos { x, y });
+            if letter == &first_letter {
+                first_letter_pos.push(GridPos { x, y });
             }
         }
     }
+    println!("Found {} at {} positions", first_letter, first_letter_pos.len());
 
-    for xxx in x_pos.iter() {
-        find_word_in_grid(&input_grid, "soop".to_string(), xxx);
+    // See if any of the X positions have the full word
+    let mut total_matching = 0;
+    for p_match_pos in first_letter_pos.iter() {
+        total_matching += find_word_in_grid(&input_grid, &search_term.to_string(), p_match_pos);
     }
-
-    // LOGIC:
-    // get positions of the first letter of the word
-    // pass to a function
-    //  find_word_in_grid(grid, word, start_pos)
-    //  which then passes it to
-    //    look up(grid, letter, start_pos)  | down, left, right, ul, ur, dl, dr
-    //    if up true for X, M, A, S then +1. Go other dirs
-
-    None
+    Some(total_matching as u32)
 }
 
 pub fn part_two(input: &str) -> Option<u32> {
-    None
+    // D.K- a horrible shoehorn of my implementation to solve the answer
+    let search_term = "MAS";
+    let input_grid = parse_into_grid(input);
+
+    let first_letter = search_term.chars().next().unwrap().to_string();
+    let mut first_letter_pos: Vec<GridPos> = Vec::new(); // [1, 2], [0, 0]...
+    for (y, row) in input_grid.iter().enumerate() {
+        for (x, letter) in row.iter().enumerate() {
+            if letter == &first_letter {
+                first_letter_pos.push(GridPos { x, y });
+            }
+        }
+    }
+    println!("Found {} at {} positions", first_letter, first_letter_pos.len());
+
+    let mut total_grid_pos: Vec<GridPos> = Vec::new();
+    for p_match_pos in first_letter_pos.iter() {
+        total_grid_pos.append(find_cross_word_in_grid(&input_grid, &search_term.to_string(), p_match_pos).as_mut());  // add newly found grid pos to our running vector
+    }
+
+    // Any duplicate pos in the vector mean theres a valid cross
+    let mut matches = 0;
+    let iter_pos_clone = total_grid_pos.clone();
+    for pos in total_grid_pos {
+        if iter_pos_clone.iter().filter(|&n| *n == pos).count() == 2 {
+            matches += 1;
+        }
+    }
+    Some(matches/2 as u32)  // each entry will be in the list twice so /2 gives real result
 }
 
 #[cfg(test)]
@@ -136,12 +273,12 @@ mod tests {
     #[test]
     fn test_part_one() {
         let result = part_one(&advent_of_code::template::read_file("examples", DAY));
-        assert_eq!(result, None);
+        assert_eq!(result, Some(18));
     }
 
     #[test]
     fn test_part_two() {
         let result = part_two(&advent_of_code::template::read_file("examples", DAY));
-        assert_eq!(result, None);
+        assert_eq!(result, Some(9));
     }
 }
